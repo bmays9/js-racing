78fdcfc97b71ae3626e42f472e96b5fb1d736774
Barry Mays <barrykmays@gmail.com>
2024-05-15 21:06:53 +0200

    Added even listener for Number of Players change

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,15 +7,33 @@ document.addEventListener("DOMContentLoaded", runGame());
 function runGame() {
     console.log("func:runGame")
 
-    // Set game parameters
+    // Setup initial game data and configuration - 1 player, 10 races.
     let numberOfPlayers = 1;
     let numberOfRaces = 10;
-    let playerNames = ["Player1", "Basa", "Cody",
-        "Darlene", "Ernest", "Fatima", "George", "Harry", "India", "Jadis"];
+    let playerNames = ["Player1", "Basa", "Carol",
+        "Dwight", "Ernest", "Fatima", "Glenn", "Herschel", "India", "Jadis"];
     let cars = ["Audi", "Mercedes", "Ferrari", "BMW", "Tesla",
         "Jaguar", "Ford", "Toyota", "Honda", "Porsche"];
 
-    // add Player 1 name to input text field
+    // add Player 1 name (playerNames[0]) to input text field
+    document.getElementById('player-name').value = playerNames[0];
+
+    //add event listeners to the 3 setup options
+    //firstly, the 2 dropdowns
+    let numPlayers = document.getElementById('number-of-players');
+    numPlayers.addEventListener("change", function () {
+        console.log("num of players changed");
+        numberOfPlayers = numPlayers.value;
+        console.log(numberOfPlayers);
+    });
+    console.log("Number of Players from within game function" + numberOfPlayers);
+
+    //document.getElementById('number-of-races').addEventListener("change", numRaces);
+    //document.getElementById('submit-button').addEventListener("click", submitChange);
+    
+}
+     
 
 
+;
 }
