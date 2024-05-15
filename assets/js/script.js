d651a8a00fff674807fd7c0d8439f2d81434388e
Barry Mays <barrykmays@gmail.com>
2024-05-15 17:42:29 +0200

    Added initial eventhandler for DOM Content, and runGame function with some starting variables defined

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,21 @@
 console.log("It's working")
+document.addEventListener("DOMContentLoaded", runGame()); 
+
+/**
+ * function called by DOMContentLoad. Contains all game code.
+ */
+function runGame() {
+    console.log("func:runGame")
+
+    // Set game parameters
+    let numberOfPlayers = 1;
+    let numberOfRaces = 10;
+    let playerNames = ["Player1", "Basa", "Cody",
+        "Darlene", "Ernest", "Fatima", "George", "Harry", "India", "Jadis"];
+    let cars = ["Audi", "Mercedes", "Ferrari", "BMW", "Tesla",
+        "Jaguar", "Ford", "Toyota", "Honda", "Porsche"];
+
+    // add Player 1 name to input text field
+
+    document.addEventListener("DOMContentLoaded", runGame());
+}
