e47e32e0994b15abb4087ca020e82c2c852fc417
Barry Mays <barrykmays@gmail.com>
2024-05-16 15:28:32 +0200

    function added: buildPlayerData used to build initial playerData array

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,32 @@
 console.log("It's working")
-document.addEventListener("DOMContentLoaded", runGame()); 
+
+// Setup initial game data and configuration - 1 player, 10 races.
+let numberOfPlayers = 1;
+let numberOfRaces = 10;
+const playersInTotal = 10;  // Number of game players, including computer controlled.
+let playerNames = ["Player1", "Basa", "Carol",
+    "Dwight", "Ernest", "Fatima", "Glenn", "Hideki", "India", "Jerry"];
+let cars = ["Audi", "Mercedes", "Ferrari", "BMW", "Tesla",
+    "Jaguar", "Ford", "Toyota", "Honda", "Porsche"];
+let countries = ["Australia", "Japan", "Germany", "Malaysia", "Hungary", "U.S.A.", "Monaco", "Brazil", "Italy", "South Africa"]
+let playerData = [];
+
+document.addEventListener("DOMContentLoaded", runGame); 
 
 /**
- * function called by DOMContentLoad. Contains all game code.
+ * function called by DOMContentLoad. Initialises the game data and waits for user action.
  */
 function runGame() {
     console.log("func:runGame")
 
-    // Setup initial game data and configuration - 1 player, 10 races.
-    let numberOfPlayers = 1;
-    let numberOfRaces = 10;
-    let playerNames = ["Player1", "Basa", "Carol",
-        "Dwight", "Ernest", "Fatima", "Glenn", "Herschel", "India", "Jadis"];
-    let cars = ["Audi", "Mercedes", "Ferrari", "BMW", "Tesla",
-        "Jaguar", "Ford", "Toyota", "Honda", "Porsche"];
-
-    // add Player 1 name (playerNames[0]) to input text field
+    // Setup initial game data and configuration - Player 1 name:
     document.getElementById('player-name').value = playerNames[0];
+
+    buildPlayerData();
+    console.log(playerData);
+
+    //Populate Game-Area Table with player details
+    
 
     //add event listeners to the 3 setup options
     //firstly, the 2 dropdowns
@@ -32,8 +42,21 @@ function runGame() {
     //document.getElementById('submit-button').addEventListener("click", submitChange);
     
 }
-     
+/**
+ * Populating initial array of playerData objects. 
+ */
+function buildPlayerData() {
+    console.log("func: buildPlayerData")
+    for (let i = 0; i < playersInTotal; i++) {
+        playerData.push({
+            "name": playerNames[i],
+            "car": cars[i],
+            "boost": 0,
+            "rating": 0,
+            "race-points": 0,
+            "season-points": 0,
+        });
+    };
+    console.log(playerData);
+};
 
-
-;
-}
